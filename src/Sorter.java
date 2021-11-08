9213a1c067c769de95707b7f5e639fef350a59ba
MohamedMedni <muhamed.medni@gmail.com>
2021-11-08 21:39:47 +0100

    Updating Sort_Main with new logic

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,2 +1,18 @@
 public class Sorter {
+    public static void sort(int[] unsorted) {
+        for (int i = 0; i < unsorted.length; i++) {
+            int temp;
+            for (int j = 0; j < unsorted.length; j++) {
+                if (unsorted[i] < unsorted[j]){
+                    temp = unsorted[i];
+                    unsorted[i] = unsorted[j];
+                    unsorted[j] = temp;
+                }
+            }
+        }
+       for (int i = 0; i < unsorted.length - 1; i++) {
+           System.out.print(unsorted[i] + " ");
+       }
+          System.out.print(unsorted[unsorted.length - 1]);
+    }
 }
