74db439188f4d634d37387dfd744244b447ec2a4
MohamedMedni <muhamed.medni@gmail.com>
2021-11-08 22:13:07 +0100

    upadted Sort_Main file

--- src/Sort_Main.java
+++ src/Sort_Main.java
@@ -5,14 +5,13 @@ public class Sort_Main {
             System.out.println();
     }
 
-
     public static void main(String[] args) {
         int row = args.length;     // Initialize the array row
         int [] arr = new int [row];
         for ( int i = 0; i < row; i++ ) {
             try {
                 arr[i] = Integer.parseInt(args[i]); // Convert integer
-            }catch (NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 System.err.println("Failed trying to parse a non-numeric argument, " + arr[i]);
                }
             }
