d7423f5435e72439e19ffb8c93b204b82f16cedb
MohamedMedni <muhamed.medni@gmail.com>
2021-11-08 21:53:45 +0100

    Updating Sort_Main.java with new logic

--- src/Sort_Main.java
+++ src/Sort_Main.java
@@ -1,2 +1,17 @@
 public class Sort_Main {
+
+    public static void main(int[] args) {
+            Sorter.sort(args); // calling sort method from Sorter.java
+            System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int row = args.length;     // Initialize the array row
+        int [] arr = new int [row];
+        for ( int i = 0; i < row; i++ ) {
+            arr[i] = Integer.parseInt(args[i]); // Convert integer
+        }
+          main(arr); // calling main method
+        }
+
 }
