92ec87f1c8fc82869fcdad4f73d04d714d9627b5
MohamedMedni <muhamed.medni@gmail.com>
2021-11-10 15:52:34 +0100

    Upadted ALL files with new logic

--- src/SortTest.java
+++ src/SortTest.java
@@ -1,51 +1,53 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class SortTest {
+    public static void main(String[] args) {
+        //Test1
+        System.out.println("Test1");
+        int[] arr1 = Sorter.sort(new int[]{55, -11, -50, 100});
+        System.out.println("expected: -50 -11 55 100");
+        System.out.print("Received: ");
+        for (int i = 0; i < arr1.length ; i++) {
+            System.out.print(arr1[i] + " ");
+        }
 
-    @Test
-    public void SortTest1() {
-        int[] arr = {2, 4, 1};
-        int[] expected = {1, 2, 4};
-        Sorter.sort(arr);
-        assertArrayEquals(expected, arr);
-    }
+        //Test2
+        System.out.println("");
+        System.out.println("Test2");
+        int[] arr2 = Sorter.sort(new int[]{76, 11, -55, 2, 16, -21, 7, 88});
+        System.out.println("expected: -55 -21 2 7 11 16 76 88");
+        System.out.print("Received: ");
+        for (int i = 0; i < arr2.length ; i++) {
+            System.out.print(arr2[i] + " ");
+        }
 
-    @Test
-    public void SortTest2() {
-        Sorter sorter= new Sorter();
-        int[] arr = {55, -11,-50,100};
-        int[] expected = {-50, -55, 55,100};
-        Sorter.sort(arr);
-        assertArrayEquals(expected, arr);
-    }
+        //Test3
+        System.out.println("");
+        System.out.println("Test3");
+        int[] arr3 = Sorter.sort(new int[]{2, 16, -21, 7, 88});
+        System.out.println("expected: -21 2 7 16 88");
+        System.out.print("Received: ");
+        for (int i = 0; i < arr3.length ; i++) {
+            System.out.print(arr3[i] + " ");
+        }
 
-    @Test
-    public void SortTest3() {
-        Sorter sorter= new Sorter();
-        int[] arr = {10,2, 33 };
-        int[] expected = {2,10,33};
-        Sorter.sort(arr);
-        assertArrayEquals(expected, arr);
+        //Test4
+        System.out.println("");
+        System.out.println("Test4");
+        int[] arr4 = Sorter.sort(new int[]{4, -1, 0, -4, 1});
+        System.out.println("expected: -4 -1 0 1 4");
+        System.out.print("Received: ");
+        for (int i = 0; i < arr4.length ; i++) {
+            System.out.print(arr4[i] + " ");
+        }
 
-    }
-
-    @Test
-    public void SortTest4() {
-        Sorter sorter= new Sorter();
-        int[] arr = {-55, 100};
-        int[] expected = {100, 100};
-        Sorter.sort(arr);
-        assertArrayEquals(expected, arr);
-    }
-
-    @Test
-    public void SortTest5() {
-        Sorter sorter= new Sorter();
-        int[] arr = {-55, -20,-1, 8, 20,40, 55,100};
-        int[] expected = {-55, -20,-1, 8, 20,40, 55,100};
-        Sorter.sort(arr);
-        assertArrayEquals(expected, arr);
+        //Test5
+        System.out.println("");
+        System.out.println("Test5");
+        int[] arr5 = Sorter.sort(new int[]{10, 1, -29, 5, 89, 22});
+        System.out.println("expected: -29 1 5 10 22 89");
+        System.out.print("Received: ");
+        for (int i = 0; i < arr5.length ; i++) {
+            System.out.print(arr5[i] + " ");
+        }
     }
 
 }
--- src/Sort_Main.java
+++ src/Sort_Main.java
@@ -1,22 +1,14 @@
 public class Sort_Main {
-
-    public static void main(int[] args) {
-            Sorter sorter = new Sorter();
-            sorter.sort(args); // calling sort method from Sorter.java
-            System.out.println();
-    }
-
     public static void main(String[] args) {
         int row = args.length;     // Initialize the array row
         int [] arr = new int [row];
         for ( int i = 0; i < row; i++ ) {
             try {
                 arr[i] = Integer.parseInt(args[i]); // Convert integer
+                System.out.print(arr[i] + " ");
             } catch (NumberFormatException e) {
                 System.err.println("Failed trying to parse a non-numeric argument, " + arr[i]);
                }
             }
-          main(arr); // calling int main method
         }
-
 }
